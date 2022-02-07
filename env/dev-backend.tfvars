8c6ecc028615389f7e6123f10403eec1485cbcf7
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-02-07 07:13:12 +0530

    Update terraform-immutable-alb/

---
+++ env/dev-backend.tfvars
@@ -0,0 +1,3 @@
+bucket = "terraform-b61"
+key    = "immutable/alb/dev/terraform.tfstate"
+region = "us-east-1"
---
+++ outputs.tf
@@ -0,0 +1,20 @@
+output "PUBLIC_LB_ARN" {
+  value = aws_lb.public.arn
+}
+
+output "PRIVATE_LB_ARN" {
+  value = aws_lb.private.arn
+}
+
+output "PRIVATE_LISTENER_ARN" {
+  value = aws_lb_listener.private.arn
+}
+
+output "PUBLIC_LB_DNSNAME" {
+  value = aws_lb.public.dns_name
+}
+
+output "PRIVATE_LB_DNSNAME" {
+  value = aws_lb.private.dns_name
+}
+
